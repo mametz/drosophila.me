b03e5d821c9634eea7eaa1317e5e9cb1a1281999
Mario Metzler <m.metzler@me.com>
2015-10-21 20:24:29 +0200

    the crosses are not calculated anymore at each view, progeny is saved in db

--- app/controllers/crosses_controller.rb
+++ app/controllers/crosses_controller.rb
@@ -10,28 +10,26 @@ class CrossesController < ApplicationController
   # GET /crosses/1
   # GET /crosses/1.json
   def show
-    @flym = Fly.find(@cross.male_id)
-    @flyf = Fly.find(@cross.female_id)
+    flym = Fly.find(@cross.male_id)
+    flyf = Fly.find(@cross.female_id)
 
-    chrm = @flym.chr1.split('/')
-    chrf = @flyf.chr1.split('/')
-    chr1 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
+    @parent_male = [flym.chr1.split('/').to_set, flym.chr2.split('/').to_set, flym.chr3.split('/').to_set, flym.chr4.split('/').to_set]
+    @parent_female = [flyf.chr1.split('/').to_set, flyf.chr2.split('/').to_set, flyf.chr3.split('/').to_set, flyf.chr4.split('/').to_set]
 
-    chrm = @flym.chr2.split('/')
-    chrf = @flyf.chr2.split('/')
-    chr2 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
+    prog = Fly.where("cross_id = ?", @cross.id)
 
-    chrm = @flym.chr3.split('/')
-    chrf = @flyf.chr3.split('/')
-    chr3 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
+    if @cross.male_id == @cross.female_id
+      prog = prog.drop(1)
+    else
+      prog = prog.drop(2)
+    end
 
-    chrm = @flym.chr4.split('/')
-    chrf = @flyf.chr4.split('/')
-    chr4 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
-
-    @progeny = chr1.product(chr2,chr3,chr4).uniq
-    @parent_male = [@flym.chr1.split('/').to_set, @flym.chr2.split('/').to_set, @flym.chr3.split('/').to_set, @flym.chr4.split('/').to_set]
-    @parent_female = [@flyf.chr1.split('/').to_set, @flyf.chr2.split('/').to_set, @flyf.chr3.split('/').to_set, @flyf.chr4.split('/').to_set]
+    @progeny = []
+    @prog_id = []
+    prog.each do |n|
+      @progeny.append([n.chr1.split('/').to_set, n.chr2.split('/').to_set, n.chr3.split('/').to_set, n.chr4.split('/').to_set])
+      @prog_id.append(n.id)
+    end
 
     @balancers = @cross.balancers.split(',')
     @lethal = @cross.lethal.split(',')
@@ -68,6 +66,37 @@ class CrossesController < ApplicationController
         @flyf = Fly.new(:chr1 => params[:f_X], :chr2 => params[:f_II], :chr3 => params[:f_III], :chr4 => params[:f_IV], :cross_id => @cross.id)
 
         if @flym.save and @flyf.save
+
+          chrm = @flym.chr1.split('/')
+          chrf = @flyf.chr1.split('/')
+          chr1 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
+
+          chrm = @flym.chr2.split('/')
+          chrf = @flyf.chr2.split('/')
+          chr2 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
+
+          chrm = @flym.chr3.split('/')
+          chrf = @flyf.chr3.split('/')
+          chr3 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
+
+          chrm = @flym.chr4.split('/')
+          chrf = @flyf.chr4.split('/')
+          chr4 = [Set[chrm[0],chrf[0]],Set[chrm[0],chrf[1]],Set[chrm[1],chrf[0]],Set[chrm[1],chrf[1]]]
+
+          progeny = chr1.product(chr2,chr3,chr4).uniq
+
+          progeny.each do |p|
+            out = []
+            p.each do |n|
+              if n.size == 1
+                out.append(n.to_a[0]+"/"+n.to_a[0])
+              else
+                out.append(n.to_a.join('/'))
+              end
+            end
+            flyp = Fly.new(:chr1 => out[0], :chr2 => out[1], :chr3 => out[2], :chr4 => out[3], :cross_id => @cross.id)
+            flyp.save
+          end
 
           @cross.update(:male_id => @flym.id, :female_id => @flyf.id)
 
