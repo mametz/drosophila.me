5643b700882b4e8cc1c36538b69b74a191e2f035
Mario Metzler <m.metzler@unibas.ch>
2015-10-27 13:09:50 +0100

    flies are not destroyed anymore, when cross is destroyed

--- app/models/cross.rb
+++ app/models/cross.rb
@@ -1,6 +1,6 @@
 class Cross < ActiveRecord::Base
 	belongs_to :user
-	has_many :flies, :dependent => :destroy
+	has_many :flies
 
 	extend FriendlyId
   	friendly_id :slug_candidates, use: [:slugged, :finders]
