2e11b63d92487617f28cf82bf25d211144918f81
Mario Metzler <m.metzler@unibas.ch>
2015-12-09 12:42:30 +0100

    it's now not possible to access crosses directly by id anymore

--- app/controllers/crosses_controller.rb
+++ app/controllers/crosses_controller.rb
@@ -265,6 +265,9 @@ class CrossesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cross
+      if params[:id].is_number?
+          redirect_to root_url
+      end
       @cross = Cross.find(params[:id])
     end
 
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Drosophila
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    Dir[File.join(Rails.root, "lib", "core_ext", "*.rb")].each {|l| require l }
   end
 end
