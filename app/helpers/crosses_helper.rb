fd4656d12d9d83864c39cfc1f1eb4d24715c9e24
Mario Metzler <m.metzler@unibas.ch>
2016-01-05 17:02:12 +0100

    lethal flies are not shown anymore in chromosome view

--- app/helpers/crosses_helper.rb
+++ app/helpers/crosses_helper.rb
@@ -37,4 +37,46 @@ module CrossesHelper
 		end
 		return out
 	end
+
+	def checkLetality(input,balancers,lethal)
+		death = false
+		input.each do |chr|
+			if chr.size > 1
+				up = chr.to_a[0].split(',')
+				down = chr.to_a[1].split(',')
+
+				up.each do |u|
+					down.each do |d|
+						if u == d
+							balancers.each do |b|
+								if u == b
+									death = true
+								end
+							end
+							lethal.each do |l|
+								if u == l
+									death = true
+								end
+							end
+						end
+					end
+				end
+			else
+				n = chr.to_a
+				balancers.each do |b|
+						if n[0] == b
+							death = true
+						end
+				end
+				lethal.each do |b|
+						if n[0] == b
+							death = true
+						end
+				end
+			end
+		end
+
+		return death
+
+	end
 end
